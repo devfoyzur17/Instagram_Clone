3fd67b29d7928fa7dce1e6dd8d9245bc043c0c4b
foyzur17 <foyzur17@gmail.com>
2022-09-23 09:14:56 +0600

    home page ui

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,11 +10,19 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final PageController controller = PageController();
+  int postImage =1;
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-
         backgroundColor: Colors.transparent,
         elevation: 0,
         title: Image.asset(
@@ -87,26 +95,131 @@ class _HomePageState extends State<HomePage> {
           Divider(
             height: 1,
           ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height-112,
-            child: ListView.builder(
-              physics: NeverScrollableScrollPhysics(),
-              itemCount: 5,
-              itemBuilder: (context, index) => Container(
-                child: Column(
-                  children: [
-                    ListTile(
-                      leading: CircleAvatar(
-                        backgroundImage: AssetImage("assets/images/demo_pic.jpg"),
-                      ),
-                      title: Text("Foyzur Rahaman"),
-                      subtitle: Text("Mirpur, Dhaka."),
-                      trailing: IconButton(onPressed: (){},icon: Icon(Icons.more_horiz,color: Colors.black87,),),
-                    )
-                  ],
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Flexible(
+                child: ListView.builder(
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: 5,
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) => Container(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage:
+                                AssetImage("assets/images/demo_pic.jpg"),
+                          ),
+                          title: Text("Foyzur Rahaman"),
+                          subtitle: Text("Mirpur, Dhaka."),
+                          trailing: IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.more_horiz,
+                              color: Colors.black87,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 400,
+                          child: Stack(
+                            children: [
+                              PageView.builder(
+
+                                onPageChanged: (value){
+                                  setState(() {
+                                    postImage = value+1;
+                                  });
+                                },
+                                physics: BouncingScrollPhysics(),
+                                itemCount: 3,
+                                itemBuilder: (context, index) =>
+                                    Image.asset("assets/images/demo_img.png"),
+                              ),
+                              Positioned(
+                                  top: 20,
+                                  right: 10,
+                                  child: Container(
+                                    padding: EdgeInsets.symmetric(horizontal: 10,vertical: 7),
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(20),
+                                      color: Colors.black54
+                                    ),
+
+                                      child: Text("$postImage/3",style: TextStyle(color: Colors.white),)))
+                            ],
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Expanded(
+                              child: Row(
+                                children: [
+                                  IconButton(
+                                      onPressed: () {},
+                                      icon: ImageIcon(
+                                        AssetImage("assets/images/Like.png"),
+                                        color: Colors.black,
+                                      )),
+                                  IconButton(
+                                      onPressed: () {},
+                                      icon: ImageIcon(
+                                        AssetImage("assets/images/Comment.png"),
+                                        color: Colors.black,
+                                      )),
+                                  IconButton(
+                                      onPressed: () {},
+                                      icon: ImageIcon(
+                                        AssetImage("assets/images/Shape.png"),
+                                        color: Colors.black,
+                                      )),
+                                ],
+                              ),
+                            ),
+                            Icon(Icons.more_horiz),
+                            Expanded(
+                              child: Row(
+                                children: [
+                                  Spacer(),
+                                  IconButton(
+                                      onPressed: () {},
+                                      icon: ImageIcon(
+                                        AssetImage("assets/images/Save.png"),
+                                        color: Colors.black,
+                                      )),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              "550 Like",
+                              textAlign: TextAlign.start,
+                            ),
+                            SizedBox(
+                              width: 15,
+                            ),
+                            Text(
+                              "50 Comments",
+                              textAlign: TextAlign.start,
+                            )
+                          ],
+                        ),
+                        Divider(),
+                      ],
+                    ),
+                  ),
                 ),
               ),
-            ),
+            ],
           )
         ],
       ),
